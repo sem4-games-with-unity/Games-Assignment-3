9df542c4a97b7f2fba5b0a7c9869c39b3b54b9dd
Nikolaj <supernikolaj@hotmail.com>
2019-02-17 20:29:55 +0100

    tweening of flight simulator done

---
+++ Assets/Scripts/RotateScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class RotateScript : MonoBehaviour
+{
+    void Update()
+    {
+        transform.DORotate(transform.rotation, 1, RotateMode.FastBeyond360);
+    }
+}
