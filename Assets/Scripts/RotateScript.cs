e03740226497d92a1665dfd169a621cbcb0ca924
Nikolaj <supernikolaj@hotmail.com>
2019-02-19 21:08:06 +0100

    TPS enemy now rotating

--- Assets/Scripts/RotateScript.cs
+++ Assets/Scripts/RotateScript.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
 
 public class RotateScript : MonoBehaviour
 {
+    public float speed;
     void Update()
     {
-        transform.DORotate(transform.rotation, 1, RotateMode.FastBeyond360);
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/TimeScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScript : MonoBehaviour
+{
+    public float startTime;
+    float timeLeft;
+    public GameObject timeText;
+
+    private void Start()
+    {
+        timeLeft = startTime;
+    }
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timeText.GetComponent<UnityEngine.UI.Text>().text = "Time: " + timeLeft;
+        if(timeLeft <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+
+    }
+}
